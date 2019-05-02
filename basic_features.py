6b94eeb4ccc36d4401d30c5d9472d60b7d258987
Matej Klemen <matej.klemen1337@gmail.com>
2019-05-02 18:24:55 +0200

    Add basic framework for running experiments

--- basic_features.py
+++ basic_features.py
@@ -1,11 +1,5 @@
 import pandas as pd
-from boxoffice_utils import fix_train_budget_revenue, fix_genres, fix_runtime, \
-    train_val_test_split, rmsle, tune_knn, tune_ridge, tune_svr, tune_rf, tune_xgb
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import Ridge
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
-import xgboost as xgb
+from boxoffice_utils import fix_train_budget_revenue, fix_genres, fix_runtime, run_models
 
 if __name__ == "__main__":
     df_offline = pd.read_csv("data/train.csv", header=0)
@@ -17,117 +11,18 @@ if __name__ == "__main__":
     # TODO: turn some other basic features into numbers (e.g. using dummy variables) and use them
     FEATS = ["runtime"]
     LABEL = ["revenue"]
-    train, val, test = train_val_test_split(df_offline, train_prop=0.7, val_prop=0.1,
-                                            test_prop=0.2, seed=1337)
-    train_X, train_y = train[FEATS], train[LABEL]
-    val_X, val_y = val[FEATS], val[LABEL]
-    test_X, test_y = test[FEATS], test[LABEL]
-    print("{} examples in training, {} in validation and {} in test set...".format(train.shape[0],
-                                                                                   val.shape[0],
-                                                                                   test.shape[0]))
 
-    # ------------------------------------------
-    # k-nearest neighbors regression
-    # ------------------------------------------
-    best_k, best_weights, best_err = tune_knn(train_X, train_y, val_X, val_y,
-                                              neigh_params=[1, 3, 5, 10, 20],
-                                              weight_params=["uniform", "distance"])
-
-    print("Winning parameters for KNN: k={:d}, weights='{:s}'... "
-          "Obtained error: {:.5f}".format(best_k, best_weights, best_err))
-    knn = KNeighborsRegressor(n_neighbors=best_k,
-                              weights=best_weights,
-                              n_jobs=-1)
-    knn.fit(pd.concat([train_X, val_X], ignore_index=True),
-            pd.concat([train_y, val_y], ignore_index=True))
-    offline_test_preds = knn.predict(test_X)
-    offline_error = rmsle(offline_test_preds, test_y)
-    print("[KNN] Offline RMSLE: {:.5f}".format(offline_error))
-
-    # ------------------------------------------
-    # ridge regression
-    # ------------------------------------------
-    best_alpha, best_err = tune_ridge(train_X, train_y, val_X, val_y,
-                                      alpha_params=[0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1,
-                                                    0.2, 0.5, 1.0, 2.0, 5.0, 10.0],
-                                      seed=1337)
-
-    print("Winning parameters for Ridge regression: alpha={:.3f}... "
-          "Obtained error: {:.5f}".format(best_alpha, best_err))
-    ridge = Ridge(alpha=best_alpha,
-                  random_state=1337)
-    ridge.fit(pd.concat([train_X, val_X], ignore_index=True),
-              pd.concat([train_y, val_y], ignore_index=True))
-    offline_test_preds = ridge.predict(test_X)
-    offline_error = rmsle(offline_test_preds, test_y)
-    print("[Ridge] Offline RMSLE: {:.5f}".format(offline_error))
-
-    # ------------------------------------------
-    # support vector regression
-    # ------------------------------------------
-    best_c, best_eps, best_err = tune_svr(train_X, train_y, val_X, val_y,
-                                          c_params=[0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5,
-                                                    1.0, 2.0, 5.0, 10.0],
-                                          eps_params=[0.0, 0.01, 0.02, 0.1, 0.2, 0.5,
-                                                      1.0, 2.0, 5.0])
-
-    print("Winning parameters for SVM regression: c={:.3f}, eps={:.2f}... "
-          "Obtained error: {:.5f}".format(best_c, best_eps, best_err))
-    svr = SVR(C=best_c,
-              epsilon=best_eps,
-              gamma="scale")
-    svr.fit(pd.concat([train_X, val_X], ignore_index=True),
-            pd.concat([train_y, val_y], ignore_index=True))
-    offline_test_preds = svr.predict(test_X)
-    offline_error = rmsle(offline_test_preds, test_y)
-    print("[SVR] Offline RMSLE: {:.5f}".format(offline_error))
-
-    # ------------------------------------------
-    # random forest regression
-    # ------------------------------------------
-    best_n_trees, best_err = tune_rf(train_X, train_y, val_X, val_y,
-                                     n_estimators_param=[50, 100, 200, 500, 1000, 2000, 5000],
-                                     seed=1337)
-
-    print("Winning parameters for RF regression: n_estimators={}... "
-          "Obtained error: {:.5f}".format(best_n_trees, best_err))
-    rf = RandomForestRegressor(n_estimators=best_n_trees,
-                               criterion="mae",
-                               random_state=1337,
-                               n_jobs=-1)
-    rf.fit(pd.concat([train_X, val_X], ignore_index=True),
-           pd.concat([train_y, val_y], ignore_index=True))
-    offline_test_preds = rf.predict(test_X)
-    offline_error = rmsle(offline_test_preds, test_y)
-    print("[RF regression] Offline RMSLE: {:.5f}".format(offline_error))
-
-    # ------------------------------------------
-    # gradient boosting regression
-    # ------------------------------------------
-    dtrain = xgb.DMatrix(train_X, label=train_y)
-    dval = xgb.DMatrix(val_X)
-    dtest = xgb.DMatrix(test_X)
-
-    best_lr, best_lambda, best_rounds, best_err = tune_xgb(train_X, train_y, val_X, val_y,
-                                                           lr_params=[0.001, 0.005, 0.1, 0.2, 0.5],
-                                                           lambda_params=[0.01, 0.05, 0.1, 0.2,
-                                                                          0.5, 1.0],
-                                                           num_rounds_params=[10, 50, 100, 200,
-                                                                              500, 1000])
-
-    print("Winning parameters for XGB regression: learning_rate={:.3f}, l2_lambda={:.2f}, "
-          "num_boosting_rounds={:d}... Obtained error: {:.5f}".format(best_lr,
-                                                                      best_lambda,
-                                                                      best_rounds,
-                                                                      best_err))
-    param = {"n_gpus": 0, "nthread": -1, "learning_rate": best_lr, "reg_lambda": best_lambda}
-    bst = xgb.train(param, xgb.DMatrix(pd.concat([train_X, val_X], ignore_index=True),
-                                       label=pd.concat([train_y, val_y], ignore_index=True)),
-                    num_boost_round=best_rounds)
-    offline_test_preds = bst.predict(dtest)
-    offline_error = rmsle(offline_test_preds, test_y)
-    print("[XGBoost regression] Offline RMSLE: {:.5f}".format(offline_error))
-
+    model_props = {"knn": {},
+                   "ridge": {},
+                   "svr": {},
+                   "rf": {},
+                   "xgb": {}}
+    run_models(df_offline, FEATS, LABEL,
+               models=model_props,
+               train_prop=0.7,
+               val_prop=0.1,
+               test_prop=0.2,
+               seed=1337)
 
 
 
--- boxoffice_utils.py
+++ boxoffice_utils.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 import numpy as np
 import xgboost as xgb
 
@@ -144,9 +145,9 @@ def tune_svr(train_X, train_y, val_X, val_y, c_params, eps_params):
     return best_c, best_eps, best_err
 
 
-def tune_rf(train_X, train_y, val_X, val_y, n_estimators_param, seed=None):
+def tune_rf(train_X, train_y, val_X, val_y, n_estimators_params, seed=None):
     best_n_trees, best_err = None, float("inf")
-    for curr_n_trees in n_estimators_param:
+    for curr_n_trees in n_estimators_params:
         print("Testing RF regression for params: n_estimators={}...".format(curr_n_trees))
         # use MAE criterion -> MSE squaring the already huge errors might cause numerical issues
         curr_rf = RandomForestRegressor(n_estimators=curr_n_trees,
@@ -190,3 +191,175 @@ def tune_xgb(train_X, train_y, val_X, val_y, lr_params, lambda_params, num_round
                     best_err = curr_error
 
     return best_lr, best_lambda, best_rounds, best_err
+
+
+def display_results(res):
+    print("--------------------------------")
+    print("Summary of obtained results...")
+
+    for model, res in res.items():
+        print("Model '{}':".format(model))
+        for prop, value in res.items():
+            print("'{}': {}".format(prop, value))
+
+
+def run_models(df_offline, feats, label, train_prop, val_prop, test_prop, models, seed=None):
+    """
+    Parameters
+    ----------
+    df_offline: pd.DataFrame
+    :param feats:
+    :param label:
+    :param train_prop:
+    :param val_prop:
+    :param test_prop:
+    :param models:
+    :param seed:
+    :return:
+    """
+    results = {}
+    train, val, test = train_val_test_split(df_offline, train_prop=train_prop, val_prop=val_prop,
+                                            test_prop=test_prop, seed=seed)
+    print("{} examples in training, {} in validation and {} in test set...".format(train.shape[0],
+                                                                                   val.shape[0],
+                                                                                   test.shape[0]))
+
+    # only use the provided features and label
+    train_X, train_y = train[feats], train[label]
+    val_X, val_y = val[feats], val[label]
+    test_X, test_y = test[feats], test[label]
+
+    knn_properties = models.get("knn", None)
+    if knn_properties is not None:
+        # user-provided or "reasonable" default parameter options
+        neigh_params = knn_properties.get("neigh_params", [1, 3, 5, 10, 20])
+        weight_params = knn_properties.get("weight_params", ["uniform", "distance"])
+
+        best_k, best_weights, best_err = tune_knn(train_X, train_y, val_X, val_y,
+                                                  neigh_params=neigh_params,
+                                                  weight_params=weight_params)
+
+        print("Winning parameters for KNN: k={:d}, weights='{:s}'... "
+              "Obtained error: {:.5f}".format(best_k, best_weights, best_err))
+        knn = KNeighborsRegressor(n_neighbors=best_k,
+                                  weights=best_weights,
+                                  n_jobs=-1)
+        knn.fit(pd.concat([train_X, val_X], ignore_index=True),
+                pd.concat([train_y, val_y], ignore_index=True))
+        offline_test_preds = knn.predict(test_X)
+        offline_error = rmsle(offline_test_preds, test_y)
+
+        print("[KNN] Offline RMSLE: {:.5f}".format(offline_error))
+        results["knn"] = {"best_neigh": best_k,
+                          "best_weight": best_weights,
+                          "offline_error": offline_error}
+
+    ridge_properies = models.get("ridge", None)
+    if ridge_properies is not None:
+        # user-provided or "reasonable" default parameter options
+        alpha_params = ridge_properies.get("alpha_params", [0.001, 0.002, 0.005, 0.01, 0.02, 0.05,
+                                                            0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0])
+
+        best_alpha, best_err = tune_ridge(train_X, train_y, val_X, val_y,
+                                          alpha_params=alpha_params,
+                                          seed=seed)
+
+        print("Winning parameters for Ridge regression: alpha={:.3f}... "
+              "Obtained error: {:.5f}".format(best_alpha, best_err))
+        ridge = Ridge(alpha=best_alpha,
+                      random_state=seed)
+        ridge.fit(pd.concat([train_X, val_X], ignore_index=True),
+                  pd.concat([train_y, val_y], ignore_index=True))
+        offline_test_preds = ridge.predict(test_X)
+        offline_error = rmsle(offline_test_preds, test_y)
+
+        print("[Ridge] Offline RMSLE: {:.5f}".format(offline_error))
+        results["ridge"] = {"best_alpha": best_alpha,
+                            "offline_error": offline_error}
+
+    svr_properties = models.get("svr", None)
+    if svr_properties is not None:
+        # user-provided or "reasonable" default parameter options
+        c_params = svr_properties.get("c_params", [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1.0,
+                                                   2.0, 5.0, 10.0])
+        eps_params = svr_properties.get("eps_params", [0.0, 0.01, 0.02, 0.1, 0.2, 0.5, 1.0,
+                                                       2.0, 5.0])
+
+        best_c, best_eps, best_err = tune_svr(train_X, train_y, val_X, val_y,
+                                              c_params=c_params,
+                                              eps_params=eps_params)
+
+        print("Winning parameters for SVM regression: c={:.3f}, eps={:.2f}... "
+              "Obtained error: {:.5f}".format(best_c, best_eps, best_err))
+        svr = SVR(C=best_c,
+                  epsilon=best_eps,
+                  gamma="scale")
+        svr.fit(pd.concat([train_X, val_X], ignore_index=True),
+                pd.concat([train_y, val_y], ignore_index=True))
+        offline_test_preds = svr.predict(test_X)
+        offline_error = rmsle(offline_test_preds, test_y)
+
+        print("[SVR] Offline RMSLE: {:.5f}".format(offline_error))
+        results["svr"] = {"best_c": best_c,
+                          "best_eps": best_eps,
+                          "offline_error": offline_error}
+
+    rf_properties = models.get("rf", None)
+    if rf_properties is not None:
+        # user-provided or "reasonable" default parameter options
+        n_estimators_params = rf_properties.get("n_estimators_params", [50, 100, 200, 500, 1000,
+                                                                        2000, 5000])
+
+        best_n_trees, best_err = tune_rf(train_X, train_y, val_X, val_y,
+                                         n_estimators_params=n_estimators_params,
+                                         seed=seed)
+
+        print("Winning parameters for RF regression: n_estimators={}... "
+              "Obtained error: {:.5f}".format(best_n_trees, best_err))
+        rf = RandomForestRegressor(n_estimators=best_n_trees,
+                                   criterion="mae",
+                                   random_state=seed,
+                                   n_jobs=-1)
+        rf.fit(pd.concat([train_X, val_X], ignore_index=True),
+               pd.concat([train_y, val_y], ignore_index=True))
+        offline_test_preds = rf.predict(test_X)
+        offline_error = rmsle(offline_test_preds, test_y)
+
+        print("[RF regression] Offline RMSLE: {:.5f}".format(offline_error))
+        results["rf"] = {"best_n_estimators": best_n_trees,
+                         "offline_error": offline_error}
+
+    xgb_properties = models.get("xgb", None)
+    if xgb_properties is not None:
+        # user-provided or "reasonable" default parameter options
+        lr_params = xgb_properties.get("lr_params", [0.001, 0.005, 0.1, 0.2, 0.5])
+        lambda_params = xgb_properties.get("lambda_params", [0.01, 0.05, 0.1, 0.2, 0.5, 1.0])
+        num_rounds_params = xgb_properties.get("num_rounds_params",
+                                               [10, 50, 100, 200, 500, 1000])
+
+        best_lr, best_lambda, best_rounds, best_err = tune_xgb(train_X, train_y, val_X, val_y,
+                                                               lr_params=lr_params,
+                                                               lambda_params=lambda_params,
+                                                               num_rounds_params=num_rounds_params)
+
+        print("Winning parameters for XGB regression: learning_rate={:.3f}, l2_lambda={:.2f}, "
+              "num_boosting_rounds={:d}... Obtained error: {:.5f}".format(best_lr,
+                                                                          best_lambda,
+                                                                          best_rounds,
+                                                                          best_err))
+        param = {"n_gpus": 0, "nthread": -1, "learning_rate": best_lr, "reg_lambda": best_lambda}
+        bst = xgb.train(param, xgb.DMatrix(pd.concat([train_X, val_X], ignore_index=True),
+                                           label=pd.concat([train_y, val_y], ignore_index=True)),
+                        num_boost_round=best_rounds)
+        offline_test_preds = bst.predict(xgb.DMatrix(test_X))
+        offline_error = rmsle(offline_test_preds, test_y)
+
+        print("[XGBoost regression] Offline RMSLE: {:.5f}".format(offline_error))
+        results["xgb"] = {"best_lr": best_lr,
+                          "best_lambda": best_lambda,
+                          "best_num_rounds": best_rounds,
+                          "offline_error": offline_error}
+
+    display_results(results)
+    return results
+
