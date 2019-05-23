d4b18d3e70742d12813d1770e50129bcff6b237c
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2019-05-23 21:00:00 +0200

    Add XGB & KNN logs and submissions for different feature sets

--- basic_features.py
+++ basic_features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn.neighbors import KNeighborsRegressor
 from boxoffice_utils import fix_train_budget_revenue, fix_genres, fix_runtime, run_models, \
     write_submission, onehot_genres, onehot_original_language, fix_broken_json_values, add_important_cast_count, \
     prod_count_comp_lang_count, top_producer_director_writer, release_day
@@ -31,14 +32,16 @@ if __name__ == "__main__":
                 genre_encoder[curr_genre] = idx_genre
                 idx_genre += 1
 
-    df_offline = fix_broken_json_values(df_offline, 'cast')
-
     # Turn variable-length genre information into fixed-size one-hot encoded attributes
     df_offline, genre_cols = onehot_genres(df_offline, genre_encoder)
     df_test, _ = onehot_genres(df_test, genre_encoder)
     FEATS.extend(genre_cols)
 
+    df_offline = fix_broken_json_values(df_offline, 'cast')
+    df_test = fix_broken_json_values(df_test, 'cast')
+
     df_offline = add_important_cast_count(df_offline)
+    df_test = add_important_cast_count(df_test)
     FEATS.extend(["important_cast_count"])
 
     # Select languages with more than 5 examples in training set
@@ -101,12 +104,13 @@ if __name__ == "__main__":
     FEATS.extend(["is_weekend"])
     FEATS.extend(weekday_cols)
 
-    model_props = {"xgb": {}}
-    # model_props = {"knn": {},
+    model_props = {
+    #                "knn": {},
     #                "ridge": {},
     #                "svr": {},
     #                "rf": {},
-    #                "xgb": {}}
+                    "xgb": {}
+    }
     run_models(df_offline, FEATS, LABEL,
                models=model_props,
                test_prop=0.2,
@@ -120,15 +124,23 @@ if __name__ == "__main__":
     # df_offline_y = df_offline[LABEL]
     # df_test_X = df_test[FEATS]
     # parameters previously tuned
+
     # bst = xgb.train({"n_gpus": 0,
     #                  "nthread": -1,
-    #                  "learning_rate": 0.005,
-    #                  "reg_lambda": 0.5},
+    #                  "learning_rate": 0.001,
+    #                  "reg_lambda": 0.01},
     #                 xgb.DMatrix(df_offline_X, label=df_offline_y),
-    #                 num_boost_round=50)
+    #                 num_boost_round=500)
     # df_test["revenue"] = bst.predict(xgb.DMatrix(df_test_X))
+
+    # knn = KNeighborsRegressor(n_neighbors=5,
+    #                             weights="distance",
+    #                             n_jobs=-1)
+    # knn.fit(df_offline_X, df_offline_y)
+    # df_test["revenue"] = knn.predict(df_test_X)
+
     # df_test = df_test[["id", "revenue"]]
-    # write_submission(df_test, "basic_feats_submission.csv")
+    # write_submission(df_test, "support_me_on_patreon.csv")
 
 
 
